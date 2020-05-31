0d2e2f3fb0a7badf3667e6a81bd04d1030472a8a
A.S. Boerema <paraiko@gmail.com>
2020-05-31 16:38:04 +0200

    working verson of script to create 90 min running average from 10m gmic files

--- gmic_runavg_90min_single10m_files.sh
+++ gmic_runavg_90min_single10m_files.sh
@@ -1,43 +1,52 @@
 #!/bin/bash
 
-recnr=051
-#FILES=/NAS/BeeNas/01_RawData/00_rawh264files/$recnr/*.h264
-inputpath="/NAS/BeeNas/03_ProcessedData/01_ghost_gmic_90min_runavg/00_ghost_jpg/$recnr"
-outputpath="$inputpath/90min_runavg"
+recNr=$1
+#FILES=/NAS/BeeNas/01_RawData/00_rawh264files/$recNr/*.h264
+inputPath="/NAS/BeeNas/03_ProcessedData/01_ghost_gmic_90min_runavg/00_ghost_jpg"
+outputPath="$inputPath/$recNr/90min_runavg"
+tempPath="$inputPath/$recNr/temp"
 fileCtr=0
 
-#find all jpgfiles in the top leveldir of the inputpath 
-FILES=`find $inputpath/$recnr/*.mkv -maxdepth 0`
+#find all jpgfiles in the top leveldir of the inputPath 
+FILES=`find $inputPath/$recNr/*.jpg -maxdepth 0`
 
 # count the amount of jpg files in the directory.
-jpgct=`find $inputpath/$recnr/*.mkv -maxdepth 0 | wc -l`
+jpgCt=`find $inputPath/$recNr/*.jpg -maxdepth 0 | wc -l`
 
-# count the amount of running_avg files in the output directory.
-runavgct=`find $outputpath/$recnr/*.jpg -maxdepth 0 | wc -l`
+#Set the filecounter
+fileCtr=0
 
-#jpgct = ls *.h264 | wc -l
-echo $jpgct
-echo $runavgct
+
+#jpgCt = ls *.h264 | wc -l
+echo $jpgCt
 
 for f in $FILES
 do
-  if [ $fileCtr -ge $jpgct ]
-  then
     echo "Processing $f file..." 
     # take action on each file. $f store current file name
   
     #get the filenema without the path and store it in a variable
     fn="$(basename -- $f)"
-    
-    nr=$(printf "%04d" $fileCtr)
-    gmic -average_video $f,0,-1,600 -n 0,255 -o $outputpath/$recnr/$fn"_"$nr.jpg
 
-    #Use mkvmerge to put the h264 file in an mkv container and match the speed of the container to the  recording frames/second
-    #mkvmerge --default-duration 0:30fps $f  -o /AristaNas/RaakData/01_VideoData/00_ActiveRecordings/$recnr/$fn.mkv
+    if [ $fileCtr -lt 8 ] 
+    then
+        nr=$(printf "%04d" $fileCtr)
+        cp $f $tempPath/$nr.jpg
+        ####mv $f $outputPath/$fn
+        
+        # add the filename to array
+        #avgFiles[$fileCtr]=$outputPath/$fn
+        #echo ${avgFiles[@]}
+    else
+        nr=$(printf "%04d" $fileCtr)
+        cp $f $tempPath/$nr.jpg
+        firstFile=$((fileCtr-8))
+        firstFile=$(printf "%04d" $firstFile)
 
-    #move the h264 files to another folder so they are not converted again.
-    #mv $f /AristaNas/Raak_Raw_Recordings/$recnr/mkv_converted/$fn
-  fi
-  fileCtr=$((fileCtr+1))
+        gmic -average_files ????.jpg,0,-1,1 -n 0,255 -o $outputPath/$fn.90mravg.jpg
+        rm $tempPath/$firstFile.jpg
+        ####gmic -average_files output.mkv,0,-1,600 -n 0,255 -o $f.jpg
+    fi
+    fileCtr=$((fileCtr+1))
+
 done
-
