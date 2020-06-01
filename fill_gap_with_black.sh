22f733d4f01a3b261f8abbfece6dd208c643c7f2
A.S. Boerema <paraiko@gmail.com>
2020-06-02 00:12:42 +0200

    change missingdata string

--- fill_gap_with_black.sh
+++ fill_gap_with_black.sh
@@ -10,6 +10,6 @@ for ((i=$startGapNr;i<=$endGapNr;++i))
 #for i in {0..5..1}
 do
     nr=$(printf "%04d" $i)
-    cp black_1920x1080.jpg $recnr"_"$nr"_missingdata.jpg"
+    cp black_1920x1080.jpg $recnr"_"$nr"_0000-00-00_0000.jpg"
     
 done
