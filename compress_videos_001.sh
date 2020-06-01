a48c8112316193f0fadaa47980636f1e641b11c2
A.S. Boerema <paraiko@gmail.com>
2020-06-01 22:23:37 +0200

    fix errors in compress script

--- compress_videos_001.sh
+++ compress_videos_001.sh
@@ -6,7 +6,7 @@ for f in $FILES
 do
     in=$f
     fileNr=$(printf "%04d" $fileCtr)
-    fout="/media/paraiko/Seagate/"$rec_nr"/$rec_nr"_"$fileNr".mkv"
+    fout="/NAS/BeeNas/05_compressed_recordings/"$rec_nr"/$rec_nr"_"$fileNr.mkv"
     echo "${fout}"
     ffmpeg -vsync 0 -hwaccel_device 0 -hwaccel cuvid -c:v h264_cuvid -i $in -an -c:v h264_nvenc -rc:v vbr_hq -cq:v 19 -bf:v 3 -b:v 2500k -maxrate:v 2000K -profile:v high $fout
 
