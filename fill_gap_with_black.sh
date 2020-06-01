427a961b971a49c7526955bd9a4b0d1e68b747d1
A.S. Boerema <paraiko@gmail.com>
2020-06-01 23:30:59 +0200

    finish missingdata script

---
+++ fill_gap_with_black.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get arguments from commandline
+
+recnr=$1
+startGapNr=$2
+endGapNr=$3
+
+for ((i=$startGapNr;i<=$endGapNr;++i))
+#for i in {0..5..1}
+do
+    nr=$(printf "%04d" $i)
+    cp black_1920x1080.jpg $recnr"_"$nr"_missingdata.jpg"
+    
+done
