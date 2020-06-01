c479b805ae0953cfa3376f78b4931db4a0f8095f
A.S. Boerema <paraiko@gmail.com>
2020-06-01 22:16:59 +0200

    small improvements to compress script

--- compress_videos_001.sh
+++ compress_videos_001.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 FILES=*.mkv
-fileCtr=46
-rec_nr=001
+fileCtr=0
+rec_nr=004
 for f in $FILES
 do
     in=$f
-    fileNr=$(printf "%03d" $fileCtr)
-    fout="/media/paraiko/Seagate/"$rec_nr"/compr_"$rec_nr"_"$fileNr".mkv"
+    fileNr=$(printf "%04d" $fileCtr)
+    fout="/media/paraiko/Seagate/"$rec_nr"/$rec_nr"_"$fileNr".mkv"
     echo "${fout}"
     ffmpeg -vsync 0 -hwaccel_device 0 -hwaccel cuvid -c:v h264_cuvid -i $in -an -c:v h264_nvenc -rc:v vbr_hq -cq:v 19 -bf:v 3 -b:v 2500k -maxrate:v 2000K -profile:v high $fout
 
--- gmic_runavg_90min_single10m_files.sh
+++ gmic_runavg_90min_single10m_files.sh
@@ -2,16 +2,16 @@
 
 recNr=$1
 #FILES=/NAS/BeeNas/01_RawData/00_rawh264files/$recNr/*.h264
-inputPath="/NAS/BeeNas/03_ProcessedData/01_ghost_gmic_90min_runavg/00_ghost_jpg"
-outputPath="$inputPath/$recNr/90min_runavg"
-tempPath="$inputPath/$recNr/temp"
+#inputPath="/NAS/BeeNas/03_ProcessedData/01_ghost_gmic_90min_runavg/00_ghost_jpg"
+#outputPath="$inputPath/$recNr/90min_runavg"
+#tempPath="$inputPath/$recNr/temp"
 fileCtr=0
 
 #find all jpgfiles in the top leveldir of the inputPath 
-FILES=`find $inputPath/$recNr/*.jpg -maxdepth 0`
+FILES=`find *.jpg -maxdepth 0`
 
 # count the amount of jpg files in the directory.
-jpgCt=`find $inputPath/$recNr/*.jpg -maxdepth 0 | wc -l`
+jpgCt=`find *.jpg -maxdepth 0 | wc -l`
 
 #Set the filecounter
 fileCtr=0
@@ -32,6 +32,7 @@ do
     then
         nr=$(printf "%04d" $fileCtr)
         cp $f $tempPath/$nr.jpg
+        gmic -average_files ????.jpg,0,-1,1 -n 0,255 -o $fn_ravg90.jpg
         ####mv $f $outputPath/$fn
         
         # add the filename to array
@@ -43,7 +44,7 @@ do
         firstFile=$((fileCtr-8))
         firstFile=$(printf "%04d" $firstFile)
 
-        gmic -average_files $tempPath/????.jpg,0,-1,1 -n 0,255 -o $outputPath/$fn.jpg
+        gmic -average_files ????.jpg,0,-1,1 -n 0,255 -o $fn_ravg90.jpg
         rm $tempPath/$firstFile.jpg
         ####gmic -average_files output.mkv,0,-1,600 -n 0,255 -o $f.jpg
     fi
