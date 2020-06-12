7d0f33ae4c9a0b76ea46269d0c11ca671b4a133c
A.S. Boerema <paraiko@gmail.com>
2020-06-12 17:03:23 +0200

    add camera crash montior script

---
+++ telegramcamerabot/beecam_telegram_monitor.py
@@ -0,0 +1,20 @@
+import telepot
+import glob
+import os
+import time
+import sys
+
+recname = sys.argv[1]
+bot = telepot.Bot('<Secret_botapi_token>')
+
+#list_of_files = glob.glob('/home/pi/Raak_Raw_Recordings/*.h264')
+list_of_files = glob.glob('/NAS/BeeNas/scripts/BeeDataProcessing/telegramcamerabot/*.h264')
+latest_file = max(list_of_files, key=os.path.getctime)
+lastfile_timestamp = os.path.getctime(latest_file)
+now = time.time()
+td = now - lastfile_timestamp
+td_min = str(int(td/60))
+
+if td > 6:
+    bot.sendMessage('<chat or channel id>', "Recording " + recname +": The last file was created: " + td_min + " min ago. Perhaps the camera has crashed should be check for a crash?" )
+
