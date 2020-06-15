cbc39f72bbdbb3a8de439326bf559bac52f3aa80
A.S. Boerema <paraiko@gmail.com>
2020-06-15 14:23:48 +0200

    als create empty missing mkv files

--- fill_gap_with_black.sh
+++ fill_gap_with_black.sh
@@ -10,6 +10,10 @@ for ((i=$startGapNr;i<=$endGapNr;++i))
 #for i in {0..5..1}
 do
     nr=$(printf "%04d" $i)
+    #create black avg files
     cp black_1920x1080.jpg $recnr"_"$nr"_missingdata.jpg"
+    
+    #also create empty mkv files
+    touch $recnr"_"$nr"_missingdata.mkv"
     
 done
