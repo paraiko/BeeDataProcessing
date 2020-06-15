75a55c337a91bcc6ca86166a7a8f313854f8026e
A.S. Boerema <paraiko@gmail.com>
2020-06-15 13:48:55 +0200

    update paths

--- processing_pipeline_v1/gmic_runavg_90min_single10m_files_active.sh
+++ processing_pipeline_v1/gmic_runavg_90min_single10m_files_active.sh
@@ -2,14 +2,14 @@
 
 recNr=$1
 #FILES=/NAS/BeeNas/01_RawData/00_rawh264files/$recNr/*.h264
-inputPath="/NAS/BeeNas/03_ProcessedData/01_ghost_gmic_90min_runavg/00_ghost_10m_jpg"
-outputPath="$inputPath/$recNr/90min_runavg"
-tempPath="$inputPath/$recNr/temp"
+inputPath="/NAS/BeeNas/03_ProcessedData/01_ghosting/00_ghost_avg-10m_jpg"
+outputPath="/NAS/BeeNas/03_ProcessedData/01_ghosting/01_ghost_ravg-90m_jpg"
+tempPath="/NAS/BeeNas/03_ProcessedData/01_ghosting/temp/ravg-90m"
 fileCtr=0
 
 #create output directories if they do not exist.
 mkdir -p $outputPath/$recNr
-mkdir -p $tempPath
+mkdir -p $tempPath/$recNr
 
 #find all jpgfiles in the top leveldir of the inputPath 
 FILES=`find $inputPath/$recNr/*.jpg -maxdepth 0`
@@ -18,7 +18,7 @@ FILES=`find $inputPath/$recNr/*.jpg -maxdepth 0`
 jpgCt=`find $inputPath/$recNr/*.jpg -maxdepth 0 | wc -l`
 
 # count the amount of already processed jpg files in the directory.
-ravgCt=`find $outputPath/*.jpg -maxdepth 0 | wc -l`
+ravgCt=`find $outputPath/$recNr/*.jpg -maxdepth 0 | wc -l`
 # increase ravgCt with 8 to compensate for the first skipped 8 files in the average
 #ravgCt=$((ravgCt+8))
 
@@ -43,17 +43,17 @@ do
         then
 
             nr=$(printf "%04d" $fileCtr)
-            cp $f $tempPath/$nr.jpg
-            gmic -average_files $tempPath/????.jpg,0,-1,1 -n 0,255 -o $outputPath/$fn
+            cp $f $tempPath/$recNr/$nr.jpg
+            gmic -average_files $tempPath/$recNr/????.jpg,0,-1,1 -n 0,255 -o $outputPath/$recNr/$fn
         else
 
             nr=$(printf "%04d" $fileCtr)
-            cp $f $tempPath/$nr.jpg
+            cp $f $tempPath/$recNr/$nr.jpg
             firstFile=$((fileCtr-8))
             firstFile=$(printf "%04d" $firstFile)
 
-            gmic -average_files $tempPath/????.jpg,0,-1,1 -n 0,255 -o $outputPath/$fn
-            rm $tempPath/$firstFile.jpg
+            gmic -average_files $tempPath/$recNr/????.jpg,0,-1,1 -n 0,255 -o $outputPath/$recNr/$fn
+            rm $tempPath/$recNr/$firstFile.jpg
             ####gmic -average_files output.mkv,0,-1,600 -n 0,255 -o $f.jpg
         fi
     fi
